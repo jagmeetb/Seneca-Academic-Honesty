f5adcbb27bfeb599b1a9f96529ddfe96990b3c56
sjmathew <shawnmathew33@gmail.com>
2017-01-31 23:24:35 -0500

    added action item 1

---
+++ Assignment 5/ActionItem/ActionItem/Controllers/EditController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionItem.Controllers
+{
+    public class EditController : Controller
+    {
+        // GET: Edit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Edit/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Edit/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Edit/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Edit/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Edit/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Edit/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Edit/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment 5/ActionItem/ActionItem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActionItem.MvcApplication" Language="C#" %>
